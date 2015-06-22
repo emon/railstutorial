10210de84be6e56f832a789f945ee54541187aa5
KOMURA Takaaki <emon@minidora.net>
2015-06-22 16:07:39 +0900

    init repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '2d2b48e48d863f374ee97d21092bc79bf1082fd6c425b78704500237fccbbcd8810c0859aa501436274d0fc710f21abf62d4aa6a944b60c488019b0ebc38cec4'
