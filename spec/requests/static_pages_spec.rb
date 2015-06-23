e813d2fec6c7cce452bd5fbc0a85a990cadbf204
KOMURA Takaaki <emon@minidora.net>
2015-06-24 06:52:45 +0900

    clean: RSpec, use "subject"

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,21 +3,14 @@ require 'spec_helper'
 describe "StaticPages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-  
+
+  subject { page }
   describe "Home page" do
     before { visit root_path }
 
-    it "should have the content 'Sample App'" do
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have basetitle" do
-      expect(page).to have_title("#{base_title}")
-    end
-
-    it "should not have a custom page title" do
-      expect(page).not_to have_title('| Home')
-    end
+    it { should have_content('Sample App') }
+    it { should have_title(base_title) }
+    it { should_not have_title('| Home') }
   end
   
   describe "Help Page" do
