0049403d5f87dee69493a3d70fc098f6b2c562ea
KOMURA Takaaki <emon@minidora.net>
2015-06-24 06:44:48 +0900

    clean: RSpec static_pages_spec.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,54 +5,53 @@ describe "StaticPages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   
   describe "Home page" do
+    before { visit root_path }
+
     it "should have the content 'Sample App'" do
-      visit home_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have basetitle" do
-      visit home_path
       expect(page).to have_title("#{base_title}")
     end
 
     it "should not have a custom page title" do
-      visit home_path
       expect(page).not_to have_title('| Home')
     end
   end
   
   describe "Help Page" do
+    before { visit help_path }
+
     it "should have the content 'Help'" do
-      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit help_path
       expect(page).to have_title("#{base_title} | Help")
     end
   end
 
   describe "About Page" do
+    before { visit about_path }
+
     it "should have the content 'About Us'" do
-      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit about_path
       expect(page).to have_title("#{base_title} | About Us")
     end
   end
 
   describe "Contact" do
+    before { visit contact_path }
+
     it "should have the content 'Contact'" do
-      visit contact_path
       expect(page).to have_content('ontact')
     end
 
     it "should have the title 'Contact'" do
-      visit contact_path
       expect(page).to have_title("#{base_title} | Contact")
     end
   end
