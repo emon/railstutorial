1999145b5cbbff5824e664ff4bfac6b0e2899c00
KOMURA Takaaki <emon@minidora.net>
2015-06-26 16:51:19 +0900

    clean: use "subject { page }" and full_title()

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,6 +5,7 @@ describe "StaticPages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   subject { page }
+
   describe "Home page" do
     before { visit root_path }
 
@@ -16,37 +17,22 @@ describe "StaticPages" do
   describe "Help Page" do
     before { visit help_path }
 
-    it "should have the content 'Help'" do
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      expect(page).to have_title("#{base_title} | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About Page" do
     before { visit about_path }
 
-    it "should have the content 'About Us'" do
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      expect(page).to have_title("#{base_title} | About Us")
-    end
+    it { should have_content('About Us') }
+    it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact" do
     before { visit contact_path }
 
-    it "should have the content 'Contact'" do
-      expect(page).to have_content('ontact')
-    end
-
-    it "should have the title 'Contact'" do
-      expect(page).to have_title("#{base_title} | Contact")
-    end
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 
 end
