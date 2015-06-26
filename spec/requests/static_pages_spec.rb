e810e66ffcbf14b7f1d407a6a3c8b6a17f9a81a4
KOMURA Takaaki <emon@minidora.net>
2015-06-26 16:36:27 +0900

    add "full_title helper"

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,7 +9,7 @@ describe "StaticPages" do
     before { visit root_path }
 
     it { should have_content('Sample App') }
-    it { should have_title(base_title) }
+    it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
   end
   
